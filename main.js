0a6297bbc700a034b5cd36ba2fd5bd16bbf2fc09
Emanuel Dajti <dajti49@gmail.com>
2019-02-20 12:30:05 +0100

    Add Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var str = "      Hello Developer";
+
+alert(str.trim());
+
+var array = [10,23,45,67,89];
+
+var isarr = Array.isArray(array);
+
+document.getElementById('demo').innerHTML = isarr;
+
+
